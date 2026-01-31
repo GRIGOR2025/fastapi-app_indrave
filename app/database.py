cc0e2aa97887776332854e3bfad3d6856b1ca2d6
GRIGOR1996 <grigorij.gogo96@mail.ru>
2026-02-01 03:31:38 +0500

    Adding a database password to the.env  file

--- app/database.py
+++ app/database.py
@@ -1,13 +1,27 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv  # Импортируем загрузчик
 
-SQLALCHEMY_DATABASE_URL = "postgresql+pg8000://postgres:945146112@127.0.0.1:5432/drive_db"
+# Загружаем переменные из файла .env в систему
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,client_encoding='utf8')
+# Читаем переменную DATABASE_URL
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Проверка: если вдруг забыли создать .env, программа выдаст понятную ошибку
+if SQLALCHEMY_DATABASE_URL is None:
+    raise ValueError("DATABASE_URL не найден в файле .env!")
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    client_encoding='utf8'
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 def get_db():
     db = SessionLocal()
     try:
